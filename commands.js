23fd1c4ce407e1218d66788086ea8fdf0fe691db
Keivan <keyvan0082@gmail.com>
2022-03-25 18:03:10 +0430

    Use `queue.play` to play a track
    Also the bot won't join the voice channel when any other command rather
    than `play` is used. Plus, the code has been prettified via
    prettier

--- commands.js
+++ commands.js
@@ -1,41 +1,44 @@
 const { Client, Message } = require("discord.js");
 const { player } = require("./server");
+
 /**
- * 
- * @param {Client} cli 
- * @param {Message} msg 
- * @param {String[]} args 
- 
+ *
+ * @param {Client} cli
+ * @param {Message} msg
+ * @param {String[]} args
  */
 
 module.exports = async (cli, msg, args, cmd) => {
-  if (cmd === "ping") {
-    msg.reply(`Your Ping : ${cli.ws.ping}`);
-  }
-  if (cmd === "play") {
-    let query = args.join(" ");
-    if (!query) return msg.reply("Please Give Me A Link Or Name :)");
-    let queue = player.createQueue(msg.guild.id, {
-      metadata: {
-        channel: msg.channel,
-      },
-    });
-
-    try {
-      if (!queue.connection) await queue.connect(msg.member.voice.channel);
-    } catch {
-      queue.destroy();
-      return await msg.reply({
-        content: "Could not join your voice channel!",
-        ephemeral: true,
-      });
+    if (cmd === "ping") {
+        msg.reply(`Your Ping : ${cli.ws.ping}`);
     }
+    if (cmd === "play") {
+        let query = args.join(" ");
+        if (!query) return msg.reply("Please Give Me A Link Or Name :)");
+        let queue = player.createQueue(msg.guild.id, {
+            metadata: {
+                channel: msg.channel,
+            },
+        });
 
-    const track = await player
-      .search(query, {
-        requestedBy: msg.author,
-      })
-      .then((x) => x.tracks[0]);
-    console.log(track);
-  }
+        try {
+            if (!queue.connection)
+                await queue.connect(msg.member.voice.channel);
+        } catch {
+            queue.destroy();
+            return await msg.reply({
+                content: "Could not join your voice channel!",
+                ephemeral: true,
+            });
+        }
+
+        const track = await player
+            .search(query, {
+                requestedBy: msg.author,
+            })
+            .then((x) => x.tracks[0]);
+        console.log(track);
+
+        queue.play(track);
+    }
 };
--- server.js
+++ server.js
@@ -1,38 +1,34 @@
 require("dotenv").config({ path: "./configs.env" });
 const token = process.env.token;
-const { Client, Intents } = require("discord.js");
-const { joinVoiceChannel } = require("@discordjs/voice");
+const { Client } = require("discord.js");
 
 const { Player } = require("discord-player");
 const cli = new Client({
-  shards: "auto",
-  restTimeOffset: 0,
-  intents: 641,
-  partials: ["CHANNEL", "MESSAGE", "REACTION", "USER"],
-});
-cli.login(token);
-cli.on("ready", () => {
-  console.log("Logged In ");
+    shards: "auto",
+    restTimeOffset: 0,
+    intents: 641,
+    partials: ["CHANNEL", "MESSAGE", "REACTION", "USER"],
 });
 
 const symbol = "-";
 const player = new Player(cli);
-module.exports = {cli , player}
-cli.on("messageCreate", (msg) => {
-  if (msg.content.startsWith(symbol)) {
-    let chnnlId = msg.member.voice.channelId;
-    if (chnnlId) {
-      const channl = cli.channels.cache.get(chnnlId);
-      const connection = joinVoiceChannel({
-        channelId: channl.id,
-        guildId: channl.guild.id,
-        adapterCreator: channl.guild.voiceAdapterCreator,
-      });
 
-      let args = msg.content.slice(symbol.length).trim().split(/ +/);
-      let cmd = args.shift().toLocaleLowerCase();
-      const commands = require("./commands");
-      commands(cli, msg, args, cmd);
+cli.on("messageCreate", (msg) => {
+    if (msg.content.startsWith(symbol)) {
+        let chnnlId = msg.member.voice.channelId;
+        if (chnnlId) {
+            let args = msg.content.slice(symbol.length).trim().split(/ +/);
+            let cmd = args.shift().toLocaleLowerCase();
+            const commands = require("./commands");
+            commands(cli, msg, args, cmd);
+        }
     }
-  }
 });
+
+cli.login(token);
+
+cli.on("ready", () => {
+    console.log("Logged In ");
+});
+
+module.exports = { cli, player };
